c25c0f00218d749458d8a1088c0f10964343d67a
TeuLeonardo <matheus.leodev@gmail.com>
2025-10-27 12:14:20 -0300

    First Commit

---
+++ src/test/java/teste/API/Teste/ApiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.API.Teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
